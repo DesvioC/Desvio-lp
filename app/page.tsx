a38697eacfc9160c1718c634061c88a07519be36
DesvioC <desvioconsciente@gmail.com>
2026-05-12 11:10:59 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { Footer } from "@/components/footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen pt-16 md:pt-20">
       <Header />
       <Hero />
       <About />
