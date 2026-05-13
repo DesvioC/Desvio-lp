25d91eb73212e5b4fe3017117cb0a371596576e3
DesvioC <desvioconsciente@gmail.com>
2026-05-13 10:58:14 -0300

    Create options.ts

---
+++ data/options.ts
@@ -0,0 +1,26 @@
+export const aromas = [
+  "Capim Limão",
+  "Bambu",
+  "Cascas e Folhas",
+  "Verde Vivo",
+  "Brisa do Mar",
+  "Flor de Cerejeira",
+  "Névoa Suave",
+  "Chá Branco",
+  "Baunilha",
+  "Coco",
+  "Cacau & Rum",
+  "Morango com Chantilly",
+  "Personalizado",
+];
+
+export const decoracoes = [
+  "Simples",
+  "Colorida",
+  "Marmorizada",
+  "Areia e Conchas",
+  "Com Cristais",
+  "Corações ou Flores em cera",
+  "Flores e Ervas",
+  "Personalizado",
+];
